0f55d1426e3cf36b4987af3296309d3a7ae4835f
Jose Sanchez Aranda <josesaranda@gmail.com>
2021-03-29 01:29:43 +0200

    Updated server initialization

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Server } from 'socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  app.listen(3000).then(() => {
+    console.log('Server listen on http://localhost:3000');
+    let io = new Server({path: '/socket'}).listen(3001);
+  });
 }
 bootstrap();
