7170159e515dfae1118ec0625662a36c2221e076
Jose Sanchez Aranda <josesaranda@gmail.com>
2021-03-29 01:28:59 +0200

    Added database

---
+++ server/src/db.ts
@@ -0,0 +1,13 @@
+export const accounts = [
+    {account: "Hello world", category: "Hello category", tag: "Hello tag", balance: 0, availableBalance: 0},
+    {account: "Hello world", category: "Hello category", tag: "Hello tag", balance: 1, availableBalance: 0},
+    {account: "Hello world", category: "Hello category", tag: "Hello tag", balance: 2, availableBalance: 0},
+    {account: "Hello world", category: "Hello category", tag: "Hello tag", balance: 3, availableBalance: 0},
+    {account: "Hello world", category: "Hello category", tag: "Hello tag", balance: 4, availableBalance: 0},
+    {account: "Hello world", category: "Hello category", tag: "Hello tag", balance: 5, availableBalance: 0},
+    {account: "Hello world", category: "Hello category", tag: "Hello tag", balance: 6, availableBalance: 0},
+    {account: "Hello world", category: "Hello category", tag: "Hello tag", balance: 7, availableBalance: 0},
+    {account: "Hello world", category: "Hello category", tag: "Hello tag", balance: 8, availableBalance: 0},
+    {account: "Hello world", category: "Hello category", tag: "Hello tag", balance: 9, availableBalance: 0},
+    {account: "Hello world", category: "Hello category", tag: "Hello tag", balance: 10, availableBalance: 0}
+];
