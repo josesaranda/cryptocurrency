c57963240daa9233b711f4f3524dce693e0b160d
Jose Sanchez Aranda <josesaranda@gmail.com>
2021-03-29 01:30:02 +0200

    Added services

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { accounts } from './db';
+
+export type Account = {
+  account: string;
+  category: string;
+  tag: string;
+  balance: number;
+  availableBalance: number;
+};
+
+const MAX_BTC_VALUE = 12000;
+const MIN_BTC_VALUE = 5000;
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  
+  findAllAccounts(): Account[] {
+    return accounts;
+  }
+
+  getExchange(): number {
+    return (Math.random() * (MAX_BTC_VALUE - MIN_BTC_VALUE)) + MIN_BTC_VALUE;
   }
 }
