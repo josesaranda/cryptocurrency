d9bec8de356e45dd05a4ed7f5db5f2ec41f2f63f
Jose Sanchez Aranda <josesaranda@gmail.com>
2021-03-29 01:29:19 +0200

    First app logic

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import { AccountService } from 'src/services/AccountService';
+import { ExchangeService } from 'src/services/ExchangeService';
+import { Account } from '../models/Account';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  displayedColumns: string[] = ['account', 'category', 'tag', 'balance', 'availableBalance'];
+  dataSource = new MatTableDataSource<Account>([]);
+ 
+  exchange?: number;
+
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(
+    private readonly accountService: AccountService,
+    private readonly exchangeService: ExchangeService
+  ){}
+
+  async ngAfterViewInit() {
+    this.exchange = (await this.getExchange()).value;
+    let accounts = await this.findAllAccounts();
+    this.dataSource.data = accounts;
+    this.dataSource.sort = this.sort;
+  }
+
+  private async getExchange(){
+    return this.exchangeService.getExchange();
+  }
+
+  private async findAllAccounts(){
+    return this.accountService.findAll();
+  }
+
+  btcToDollars(btc: number): string {
+    return (btc * this.exchange!).toFixed(2);
+  }
 }
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -3,6 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
+import {MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
+import { FetchService } from 'src/services/FetchService';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { AccountService } from 'src/services/AccountService';
+import { ExchangeService } from 'src/services/ExchangeService';
 
 @NgModule({
   declarations: [
@@ -10,9 +21,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatSortModule,
+    MatToolbarModule,
+    MatIconModule,
+    HttpClientModule 
   ],
-  providers: [],
+  providers: [AccountService, ExchangeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
