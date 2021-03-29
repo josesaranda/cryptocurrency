067c3ae27b59754869dabe3791f35402eb599311
Jose Sanchez Aranda <josesaranda@gmail.com>
2021-03-29 20:31:02 +0200

    Added socket to get changes from one account

--- app/src/services/AccountService.ts
+++ app/src/services/AccountService.ts
@@ -3,20 +3,35 @@ import { Injectable } from "@angular/core";
 import { FetchService } from "./FetchService";
 import { Account } from '../models/Account';
 import { Service } from "./Service";
+import { BehaviorSubject } from "rxjs";
+import { SocketService } from "./SocketService";
 
 @Injectable({
     providedIn: 'root'
 })
 export class AccountService implements Partial<Service<Account>> {
-
-    fetchService!: FetchService<Account>;
+    private changedAccount = new BehaviorSubject<Account | undefined>(undefined);
+    private fetchService!: FetchService<Account>;
 
     constructor(httpClient: HttpClient){
         this.fetchService = new FetchService<Account>(httpClient);
         this.fetchService.init('accounts');
+        this.socketAccount();
     }
 
     findAll(): Promise<Account[]> {
         return this.fetchService.findAll();
+    }
+
+    socketAccount(){
+        let socketService = SocketService.getInstance();
+        socketService.connect();
+        socketService.onNewAccount(value => {
+            this.changedAccount.next(value);
+        });
+    }
+
+    onChangeAccount(): BehaviorSubject<Account | undefined> {
+        return this.changedAccount;
     }
 }
