94f181dbfb9c79c06e268bddeaff941bb367698c
Jose Sanchez Aranda <josesaranda@gmail.com>
2021-03-29 20:31:19 +0200

    Added socket to get new exchange

--- app/src/services/ExchangeService.ts
+++ app/src/services/ExchangeService.ts
@@ -1,20 +1,39 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { BehaviorSubject, Subject } from "rxjs";
 import { FetchService } from "./FetchService";
+import { SocketService } from "./SocketService";
 
 @Injectable({
     providedIn: 'root'
 })
 export class ExchangeService {
-    
-    fetchService!: FetchService<{value: number}>;
+    private exchange = new BehaviorSubject<number | undefined>(undefined);
+    private fetchService!: FetchService<{value: number}>;
 
     constructor(httpClient: HttpClient){
         this.fetchService = new FetchService<{value: number}>(httpClient);
         this.fetchService.init('');
+        this.socketExchange();
     }
 
-    getExchange(){
-        return this.fetchService.fetch<{value: number}>('get', 'exchange');
+    async getExchange(){
+        if(this.exchange.getValue() === undefined){
+            let { value } = await this.fetchService.fetch<{value: number}>('get', 'exchange');
+            this.exchange.next(value);
+        }
+        return this.exchange.getValue();
+    }
+
+    socketExchange(){
+        let socketService = SocketService.getInstance();
+        socketService.connect();
+        socketService.onNewExchange(value => {
+            this.exchange.next(value);
+        });
+    }
+
+    subscribeToExchange(): BehaviorSubject<number | undefined>{
+        return this.exchange;
     }
 }
