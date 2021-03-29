19978e7817605b75f1453e151e1c75c1990a0252
Jose Sanchez Aranda <josesaranda@gmail.com>
2021-03-29 20:32:05 +0200

    Added some functions

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { accounts } from './db';
+import { accounts as accountsFromDB } from './db';
 
 export type Account = {
+  id: number;
   account: string;
   category: string;
   tag: string;
@@ -14,9 +15,28 @@ const MIN_BTC_VALUE = 5000;
 
 @Injectable()
 export class AppService {
+
+  private accounts: Account[] = accountsFromDB;
   
   findAllAccounts(): Account[] {
-    return accounts;
+    return this.accounts;
+  }
+
+  findOneAccount(id: number){
+    return this.accounts.find(acc => acc.id === id);
+  }
+
+  findRandomAccount(): Account {
+    let random = Math.floor(Math.random() * this.accounts.length);
+    return this.accounts[random];
+  }
+
+  updateAccount(id: number, account: Omit<Partial<Account>, 'id'>){
+    let newAccount = this.findOneAccount(id);
+    newAccount = {...newAccount, ...account};
+    let index = this.accounts.findIndex(acc => acc.id === id);
+    this.accounts[index] = newAccount;
+    return newAccount;
   }
 
   getExchange(): number {
