918a3950759dea21eb41f8a493b115ee584d40d2
Jose Sanchez Aranda <josesaranda@gmail.com>
2021-03-29 20:31:46 +0200

    Added socket service

---
+++ app/src/services/SocketService.ts
@@ -0,0 +1,43 @@
+import { Socket, io } from 'socket.io-client';
+import { Account } from '../models/Account';
+
+export class SocketService {
+    private static instance?: SocketService;
+
+    private socket?: Socket;
+
+    constructor(){}
+
+    public static getInstance(): SocketService {
+        if (!SocketService.instance) {
+            SocketService.instance = new SocketService();
+        }
+
+        return SocketService.instance;
+    }
+
+    public connect(): void {
+        if(!this.socket)
+            this.socket = io("http://localhost:3001", {path: '/socket'}).connect();
+    }
+
+    public onNewExchange(callback: (value: number) => void){
+        this.checkConnection();
+        this.socket!.on('new:exchange', (value: number) => {
+            callback(value);
+        });
+    }
+
+    public onNewAccount(callback: (value: Account) => void){
+        this.checkConnection();
+        console.log('on here');
+        this.socket!.on('new:balance', (value: Account) => {
+            callback(value);
+        });
+    }
+
+    private checkConnection(){
+        if(!this.socket)
+            throw new Error('Must call connect before run this function');
+    }
+}
