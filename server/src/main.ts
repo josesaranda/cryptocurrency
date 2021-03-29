ba9f8718c37fba103f9e68303b1d5fe7f4f73c8c
Jose Sanchez Aranda <josesaranda@gmail.com>
2021-03-29 20:32:35 +0200

    Added first try

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,13 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Server } from 'socket.io';
+import { AppService } from './app.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const appService = new AppService();
   app.enableCors();
   app.listen(3000).then(() => {
-    console.log('Server listen on http://localhost:3000');
-    let io = new Server({path: '/socket'}).listen(3001);
+    console.log('Server listening on http://localhost:3000');
+    let io = new Server({path: '/socket', cors: {
+      origin: '*',
+      methods: ['GET']
+    }}).listen(3001);
+    console.log('Socket listening on http://localhost:3001/socket');
+    setInterval(() => {
+      io.emit('new:exchange', appService.getExchange());
+    },10000);
+    setInterval(() => {
+      let account = appService.findRandomAccount();
+      let randomBalance = Math.random() * 15;
+      account.balance = randomBalance;
+      account.availableBalance = randomBalance;
+      let { id, ...rest} = account;
+      appService.updateAccount(id, rest);
+      io.emit('new:balance', account);
+    },12000);
   });
 }
 bootstrap();
