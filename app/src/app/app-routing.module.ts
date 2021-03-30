988783ece6cc291df7fc24ab55aeef54b5a65cc7
Jose Sanchez Aranda <josesaranda@gmail.com>
2021-03-31 00:14:25 +0200

    Added guard

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccountsComponent } from 'src/pages/accounts/accounts.component';
 import { DetailsComponent } from 'src/pages/details/details.component';
+import { DetailsGuard } from 'src/pages/details/details.guard';
 
 const routes: Routes = [
   {path: 'accounts', component: AccountsComponent},
-  {path: 'accounts/:id/details', component: DetailsComponent},
+  {path: 'accounts/:id/details', component: DetailsComponent, canActivate: [DetailsGuard]},
   {path: '', redirectTo: '/accounts', pathMatch: 'full'},
 ];
 
---
+++ app/src/pages/details/details.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
+import { AccountService } from "src/services/AccountService";
+
+@Injectable()
+export class DetailsGuard implements CanActivate {
+
+    constructor(
+        private accountService: AccountService,
+        private router: Router
+    ){}
+
+    async canActivate(route: ActivatedRouteSnapshot): Promise<boolean> {
+        let {id} = route.params;
+        if(id === undefined){
+            this.router.navigateByUrl('/accounts');
+            return false;
+        }
+        try{
+            await this.accountService.findOne(id);
+            return true;
+        }catch{
+            this.router.navigateByUrl('/accounts');
+            return false;
+        }
+    }
+
+}
